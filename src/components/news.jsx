a61e39211bf341c57ba3b9d60fbd634c3b5ce74f
slick-coding <alick.doggett@yahoo.co.uk>
2025-04-03 13:38:39 +0100

    add /news path with basic styling

--- src/components/news.jsx
+++ src/components/news.jsx
@@ -2,7 +2,7 @@ import ArticleCard from "./article-card";
 
 function News({articles}) {
     
-    return <section>
+    return <section className="news">
         {articles.map((article) => {
             return <ArticleCard key={article.article_id} article={article}/>
         })}
