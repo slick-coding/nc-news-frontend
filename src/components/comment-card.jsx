af0d2362d1a4029598eebeca9458fe1cec2ec503
slick-coding <alick.doggett@yahoo.co.uk>
2025-04-04 15:45:41 +0100

    add basic comment deletion

--- src/components/comment-card.jsx
+++ src/components/comment-card.jsx
@@ -1,5 +1,12 @@
+import { useContext, useState } from "react";
+import { UserContext } from "../contexts/user";
+import { deleteComment } from "../api";
+
 function CommentCard({ comment }) {
+    const [deletionStatus, setDeletionStatus] = useState(0);
     let date = "";
+
+    const votes = comment.votes ? comment.votes : 0;
 
     if (comment.created_at) {
         date = new Date(comment.created_at).toString().split(" ").slice(0, 5).join(" ");
@@ -7,12 +14,24 @@ function CommentCard({ comment }) {
         date = "Just posted.";
     }
 
+    const {
+        userProfile: { username },
+    } = useContext(UserContext);
+
+    function handleDelete(event) {
+        event.preventDefault();
+
+        deleteComment(comment.comment_id)
+    }
+
     return (
         <section className="comment-card">
-            <h4>{comment.author}</h4>
-            <p>{comment.body}</p>
-            {comment.votes ? <p>{comment.votes} upvotes</p> : <p>0 upvotes.</p>}
-            <p>{date}</p>
+            {deletionStatus ? <h4>Comment deleted!</h4> : null}
+            {!deletionStatus ? <h4>{comment.author}</h4> : null}
+            {!deletionStatus ? <p>{comment.body}</p> : null}
+            {!deletionStatus ? <p>{votes} upvotes</p> : null}
+            {!deletionStatus ? <p>{date}</p> : null}
+            {comment.author === username && !deletionStatus ? <button onClick={handleDelete}>Delete Comment ❌</button> : null}
         </section>
     );
 }
